ac67848eb8e86ea906b458dd8fa785fde6348490
JuanPelinco <pelincojuan3@gmail.com>
2025-08-23 18:35:24 -0500

    MI SEGUNDA CHAMBA

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("Iniciando Aplicacion MVP");
+        AsistenciaApplication.main(args);
+    }
+}

0caa6e409ceeecd902f3f8721d12acd829d011a9
JuanPelinco <pelincojuan3@gmail.com>
2025-08-29 16:41:58 -0500

    NOSE MANO

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/servicio/EstudianteServicioI.java
@@ -0,0 +1,15 @@
+package pe.edu.upeu.asistencia.servicio;
+
+import pe.edu.upeu.asistencia.modelo.Estudiante;
+
+import java.util.List;
+
+public interface EstudianteServicioI {
+    void save(Estudiante estudiante);
+    List<Estudiante> findAll();
+    Estudiante update(Estudiante estudiante, int index);
+    void delete(int index);
+
+    Estudiante findById(int index);
+
+}

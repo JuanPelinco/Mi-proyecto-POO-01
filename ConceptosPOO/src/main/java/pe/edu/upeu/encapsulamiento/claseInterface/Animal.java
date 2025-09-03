d081e3fe230abda6dd4039ae88e952f265e46280
JuanPelinco <pelincojuan3@gmail.com>
2025-09-03 10:04:34 -0500

    TAREA 2.2

--- ConceptosPOO/src/main/java/pe/edu/upeu/encapsulamiento/claseInterface/Animal.java
+++
@@ -1,6 +0,0 @@
-package pe.edu.upeu.encapsulamiento.claseInterface;
-
-public interface Animal {
-    void emitirSonido();
-    void dormir();
-}
--- ConceptosPOO/src/main/java/pe/edu/upeu/polimorfismo/Loro.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.polimorfismo;
-
-public class Loro extends Animal {
-    @Override
-
-    public void emitirSonido(){
-        System.out.println("El loro dice: UN SALUDO MA' MI GENTE!! ");
-    }
-}
